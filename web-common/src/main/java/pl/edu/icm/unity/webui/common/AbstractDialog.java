321fb49e1ebeff97cd9c3f047e0684d5ff7e3196
Krzysztof Benedyczak <golbi@unity-idm.eu>
2013-12-05 20:32:18 +0100

    Dialogs got a default keyboard shortcuts (escape and enter) also focus is put on the first editable field.

--- web-common/src/main/java/pl/edu/icm/unity/webui/common/AbstractDialog.java
+++ web-common/src/main/java/pl/edu/icm/unity/webui/common/AbstractDialog.java
@@ -4,13 +4,18 @@
  */
 package pl.edu.icm.unity.webui.common;
 
+import java.util.Iterator;
+
 import pl.edu.icm.unity.server.utils.UnityMessageSource;
 
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.ui.AbstractField;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.Panel;
@@ -20,7 +25,7 @@ import com.vaadin.ui.Window;
 
 
 /**
- * Base for the dialog windows. Provides boilerplate code. In particular the overal layout is created
+ * Base for the dialog windows. Provides boilerplate code. In particular the overall layout is created
  * with two buttons at the bottom: one responsible for confirm and another for cancel.
  * <p>
  * Usage pattern: extend, provide onConfirm() logic and custom contents in getContents(). Then call show();
@@ -34,6 +39,8 @@ public abstract class AbstractDialog extends Window implements Button.ClickListe
 	private static final long serialVersionUID = 1L;
 	private Button confirm;
 	private Button cancel;
+	private Button enterButton;
+	private Button escapeButton;
 	protected Component contentsComponent;
 	protected UnityMessageSource msg;
 	protected boolean defaultSizeUndfined = false;
@@ -74,10 +81,60 @@ public abstract class AbstractDialog extends Window implements Button.ClickListe
 	protected abstract Component getContents() throws Exception;
 	protected abstract void onConfirm();
 	
+	/**
+	 * Element returned by this method gets an initial focus.
+	 * 
+	 * @return by default returns the first {@link AbstractField} which is found in the dialog contents
+	 * or null if none is found. Override to set the initial focus on other element.
+	 */
+	protected AbstractField<?> getFocussedComponent()
+	{
+		return getFocussedComponentRec(contentsComponent);
+	}
+
+	private AbstractField<?> getFocussedComponentRec(Component c)
+	{
+		if (!(c instanceof ComponentContainer))
+			return null;
+		ComponentContainer container = (ComponentContainer) c;
+		Iterator<Component> components = container.iterator();
+		while(components.hasNext())
+		{
+			Component cc = components.next();
+			if (cc instanceof AbstractField)
+				return (AbstractField<?>) cc;
+			if (cc instanceof ComponentContainer)
+				return getFocussedComponentRec(cc);
+		}
+		return null;
+	}
+	
+	/**
+	 * @return a button that is bound to the enter key or null if no button should be bound to Enter. By default
+	 * the confirm button is returned.
+	 */
+	protected Button getDefaultOKButton()
+	{
+		return (getDefaultCancelButton()==confirm) ? null : confirm;
+	}
+	
+	/**
+	 * @return a button that is bound to the Escape key or null if no button should be bound. By default
+	 * the cancel button is returned if present, if not - then the confirm button 
+	 * (which is typically the 'close' button then).
+	 */
+	protected Button getDefaultCancelButton()
+	{
+		return cancel == null ? confirm : cancel;
+	}
+	
+	
 	protected void onCancel()
 	{
 		close();
 	}
+	
+	
 	
 	protected void initGUI(boolean sizeUndefined) throws Exception
 	{
@@ -111,6 +168,13 @@ public abstract class AbstractDialog extends Window implements Button.ClickListe
 		if (!sizeUndefined)
 			vl.setSizeFull();
 		setContent(vl);
+		
+		enterButton = getDefaultOKButton();
+		if (enterButton != null)
+			enterButton.setClickShortcut(KeyCode.ENTER);
+		escapeButton = getDefaultCancelButton();
+		if (escapeButton != null)
+			escapeButton.setClickShortcut(KeyCode.ESCAPE);
 	}
 	
 	protected AbstractOrderedLayout getButtonsBar()
@@ -136,6 +200,10 @@ public abstract class AbstractDialog extends Window implements Button.ClickListe
 			return;
 		}
 		UI.getCurrent().addWindow(this);
+		focus();
+		AbstractField<?> toFocus = getFocussedComponent();
+		if (toFocus != null)
+			toFocus.focus();
 	}
 	
 	public void show()
@@ -145,6 +213,10 @@ public abstract class AbstractDialog extends Window implements Button.ClickListe
 	
 	public void close()
 	{
+		if (enterButton != null)
+			enterButton.removeClickShortcut();
+		if (escapeButton != null)
+			escapeButton.removeClickShortcut();
 		if (getParent() != null)
 			((UI) getParent()).removeWindow(this);
 	}
