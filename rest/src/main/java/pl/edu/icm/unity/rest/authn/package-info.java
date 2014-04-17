97b9775812692e76a07335713d99c46d890177de
Krzysztof Benedyczak <golbi@unity-idm.eu>
2014-04-17 13:25:29 +0200

    Skeleton of a restful binding integration layer

---
+++ rest/src/main/java/pl/edu/icm/unity/rest/authn/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2014 ICM Uniwersytet Warszawski All rights reserved.
+ * See LICENCE file for licensing information.
+ *
+ * Created on 17 kwi 2014
+ * Author: K. Benedyczak
+ */
+
+/**
+ * Authentication subsystem of the RESTful binding
+ * @author K. Benedyczak
+ */
+package pl.edu.icm.unity.rest.authn;
