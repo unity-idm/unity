6d194e4434df6e2234f58b3cc5c8664e1620c386
Krzysztof Benedyczak <golbi@unity-idm.eu>
2014-05-27 00:46:27 +0200

    use cxf 2.7 again (ws module tests failing). Fix test of REST.

--- rest/src/test/java/pl/edu/icm/unity/rest/TestRESTCore.java
+++ rest/src/test/java/pl/edu/icm/unity/rest/TestRESTCore.java
@@ -14,8 +14,15 @@ import java.util.Set;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
@@ -24,22 +31,35 @@ import eu.emi.security.authn.x509.impl.KeystoreCredential;
 import eu.unicore.util.httpclient.DefaultClientConfiguration;
 import eu.unicore.util.httpclient.HttpUtils;
 import pl.edu.icm.unity.engine.DBIntegrationTestBase;
+import pl.edu.icm.unity.rest.authn.AuthenticationInterceptor;
 import pl.edu.icm.unity.stdext.credential.CertificateVerificatorFactory;
+import pl.edu.icm.unity.stdext.credential.PasswordToken;
 import pl.edu.icm.unity.stdext.credential.PasswordVerificatorFactory;
+import pl.edu.icm.unity.stdext.identity.UsernameIdentity;
+import pl.edu.icm.unity.stdext.identity.X500Identity;
+import pl.edu.icm.unity.types.EntityState;
 import pl.edu.icm.unity.types.authn.AuthenticationRealm;
 import pl.edu.icm.unity.types.authn.AuthenticatorSet;
 import pl.edu.icm.unity.types.authn.CredentialDefinition;
 import pl.edu.icm.unity.types.authn.CredentialRequirements;
+import pl.edu.icm.unity.types.basic.EntityParam;
+import pl.edu.icm.unity.types.basic.Identity;
+import pl.edu.icm.unity.types.basic.IdentityParam;
 import pl.edu.icm.unity.types.endpoint.EndpointDescription;
 import pl.edu.icm.unity.types.endpoint.EndpointTypeDescription;
 
-
+/**
+ * Tests REST integration basic. Not that more detailed tests of {@link AuthenticationInterceptor}
+ * are in the ws module for historical reasons.  
+ * @author K. Benedyczak
+ */
 public class TestRESTCore extends DBIntegrationTestBase
 {
 	@Test
 	public void test() throws Exception
 	{
 		setupMockAuthn();
+		createUsers();
 		AuthenticationRealm realm = new AuthenticationRealm("testr", "", 
 				10, 100, -1, 600);
 		realmsMan.addRealm(realm);
@@ -63,19 +83,44 @@ public class TestRESTCore extends DBIntegrationTestBase
 		clientCfg.setValidator(new KeystoreCertChainValidator("src/test/resources/demoTruststore.jks", 
 				"unicore".toCharArray(), "JKS", -1));
 		clientCfg.setSslEnabled(true);
-
-		clientCfg.setHttpUser("user1");
-		clientCfg.setHttpPassword("mockPassword1");
 		clientCfg.setSslAuthn(false);
 		clientCfg.setHttpAuthn(true);
+		DefaultHttpClient client = (DefaultHttpClient) HttpUtils.createClient("https://localhost:53456", clientCfg);
 
-		HttpClient client = HttpUtils.createClient("https://localhost:53456", clientCfg);
 		HttpHost host = new HttpHost("localhost", 53456, "https");
+		client.getCredentialsProvider().setCredentials(
+	                new AuthScope(host.getHostName(), host.getPort()),
+	                new UsernamePasswordCredentials("user1", "mockPassword1"));
+	        AuthCache authCache = new BasicAuthCache();
+	        BasicScheme basicAuth = new BasicScheme();
+	        authCache.put(host, basicAuth);
+	        BasicHttpContext localcontext = new BasicHttpContext();
+	        localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
 		
 		HttpGet get = new HttpGet("/mock/mock-rest/test/r1");
-		HttpResponse response = client.execute(host, get);
+		HttpResponse response = client.execute(host, get, localcontext);
 		assertEquals(response.getStatusLine().toString(), 200, response.getStatusLine().getStatusCode());
 		System.out.println(EntityUtils.toString(response.getEntity()));
+		
+		//no password, should fail.
+		HttpResponse response2 = client.execute(host, get);
+		assertEquals(response2.getStatusLine().toString(), 500, response2.getStatusLine().getStatusCode());
+	}
+	
+	
+	protected void createUsers() throws Exception
+	{
+		Identity added1 = idsMan.addEntity(new IdentityParam(UsernameIdentity.ID, "user1", true), 
+				"cr-pass", EntityState.valid, false);
+		idsMan.setEntityCredential(new EntityParam(added1), "credential1", 
+				new PasswordToken("mockPassword1").toJson());
+		
+		Identity added2 = idsMan.addEntity(new IdentityParam(UsernameIdentity.ID, "user2", true), 
+				"cr-certpass", EntityState.valid, false);
+		idsMan.addIdentity(new IdentityParam(X500Identity.ID, "CN=Test UVOS,O=UNICORE,C=EU", true), 
+				new EntityParam(added2), false);
+		idsMan.setEntityCredential(new EntityParam(added2), "credential1", 
+				new PasswordToken("mockPassword2").toJson());
 	}
 	
 	protected void setupMockAuthn() throws Exception
