f130df1a0f80176be52ff55f2932b2ac4c5a3c7a
Krzysztof Benedyczak <golbi@unity-idm.eu>
2014-12-30 10:06:08 +0100

    fixed #339 fixed #341 metadata generation and defults usage is fixed, tests added

--- saml/src/test/java/pl/edu/icm/unity/samlidp/TestSamlConfiguration.java
+++ saml/src/test/java/pl/edu/icm/unity/samlidp/TestSamlConfiguration.java
@@ -6,40 +6,29 @@ package pl.edu.icm.unity.samlidp;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static pl.edu.icm.unity.saml.SamlProperties.IDENTITY_LOCAL;
+import static pl.edu.icm.unity.saml.SamlProperties.IDENTITY_MAPPING_PFX;
+import static pl.edu.icm.unity.saml.SamlProperties.IDENTITY_SAML;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.CREDENTIAL;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.DEFAULT_GROUP;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.GROUP;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.GROUP_PFX;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.GROUP_TARGET;
-import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.IDENTITY_LOCAL;
-import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.IDENTITY_MAPPING_PFX;
-import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.IDENTITY_SAML;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.ISSUER_URI;
 import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.P;
 
-import java.security.cert.X509Certificate;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Properties;
-import java.util.Set;
 
 import org.junit.Test;
 
-import pl.edu.icm.unity.exceptions.EngineException;
-import pl.edu.icm.unity.exceptions.InternalException;
-import pl.edu.icm.unity.exceptions.WrongArgumentException;
+import pl.edu.icm.unity.saml.SimplePKIManagement;
 import pl.edu.icm.unity.saml.idp.GroupChooser;
 import pl.edu.icm.unity.saml.idp.IdentityTypeMapper;
 import pl.edu.icm.unity.saml.idp.SamlIdpProperties;
-import pl.edu.icm.unity.server.api.PKIManagement;
 import pl.edu.icm.unity.stdext.identity.TargetedPersistentIdentity;
 import pl.edu.icm.unity.stdext.identity.X500Identity;
-import eu.emi.security.authn.x509.X509CertChainValidatorExt;
-import eu.emi.security.authn.x509.X509Credential;
-import eu.emi.security.authn.x509.impl.KeystoreCredential;
 import eu.unicore.samly2.SAMLConstants;
 import eu.unicore.samly2.exceptions.SAMLRequesterException;
-import eu.unicore.security.canl.IAuthnAndTrustConfiguration;
 
 public class TestSamlConfiguration
 {
@@ -55,7 +44,7 @@ public class TestSamlConfiguration
 		p.setProperty(P+GROUP_PFX+"2."+GROUP, "/");
 		p.setProperty(P+DEFAULT_GROUP, "/def");
 		p.setProperty(P+CREDENTIAL, "MAIN");
-		SamlIdpProperties cfg = new SamlIdpProperties(p, getPKIManagement());
+		SamlIdpProperties cfg = new SamlIdpProperties(p, new SimplePKIManagement());
 		
 		GroupChooser chooser = cfg.getGroupChooser();
 		assertEquals("/some/gr", chooser.chooseGroup("http://sp.org1"));
@@ -77,7 +66,7 @@ public class TestSamlConfiguration
 		p.setProperty(P+IDENTITY_MAPPING_PFX+"2."+IDENTITY_SAML, SAMLConstants.NFORMAT_TRANSIENT);
 		p.setProperty(P+IDENTITY_MAPPING_PFX+"3."+IDENTITY_LOCAL, "");
 		p.setProperty(P+IDENTITY_MAPPING_PFX+"3."+IDENTITY_SAML, "unity:identifier");
-		SamlIdpProperties cfg = new SamlIdpProperties(p, getPKIManagement());
+		SamlIdpProperties cfg = new SamlIdpProperties(p, new SimplePKIManagement());
 
 		IdentityTypeMapper idMapper = cfg.getIdTypeMapper();
 		assertEquals("qqq", idMapper.mapIdentity("123"));
@@ -95,79 +84,5 @@ public class TestSamlConfiguration
 		}
 		
 		assertEquals(idMapper.getSupportedIdentityTypes().toString(), 7, idMapper.getSupportedIdentityTypes().size());
-	}
-
-	private PKIManagement getPKIManagement()
-	{
-		return new PKIManagement()
-		{
-			@Override
-			public Set<String> getValidatorNames() throws EngineException
-			{
-				return new HashSet<>();
-			}
-			
-			@Override
-			public X509CertChainValidatorExt getValidator(String name) throws EngineException
-			{
-				throw new WrongArgumentException("No such validator " + name);
-			}
-			
-			@Override
-			public Set<String> getCredentialNames() throws EngineException
-			{
-				return Collections.singleton("MAIN");
-			}
-			@Override
-			public X509Credential getCredential(String name) throws EngineException
-			{
-				if (name.equals("MAIN"))
-					try
-					{
-						return new KeystoreCredential("src/test/resources/demoKeystore.p12",
-								"the!uvos".toCharArray(), "the!uvos".toCharArray(), 
-								null, "PKCS12");
-					} catch (Exception e)
-					{
-						throw new InternalException("error loading credential", e);
-					}
-				throw new WrongArgumentException("No such validator " + name);				
-			}
-
-			@Override
-			public IAuthnAndTrustConfiguration getMainAuthnAndTrust()
-			{
-				return null;
-			}
-
-			@Override
-			public Set<String> getCertificateNames() throws EngineException
-			{
-				return null;
-			}
-
-			@Override
-			public X509Certificate getCertificate(String name) throws EngineException
-			{
-				return null;
-			}
-
-			@Override
-			public void updateCertificate(String name, X509Certificate updated)
-					throws EngineException
-			{
-			}
-
-			@Override
-			public void removeCertificate(String name) throws EngineException
-			{
-			}
-
-			@Override
-			public void addCertificate(String name, X509Certificate updated)
-					throws EngineException
-			{
-			}
-		};
 	}
 }
