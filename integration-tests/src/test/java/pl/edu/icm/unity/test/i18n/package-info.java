b29362e486c61761caec78cdc7b36959085bd640
Krzysztof Benedyczak <golbi@unity-idm.eu>
2015-01-22 15:42:02 +0100

    Better organization of messages in web-admin (home ui related are separated). Added an integration test showing translation status on selected message bundles. Added missing translations in core and web-common.

---
+++ integration-tests/src/test/java/pl/edu/icm/unity/test/i18n/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 ICM Uniwersytet Warszawski All rights reserved.
+ * See LICENCE file for licensing information.
+ *
+ * Created on 22 sty 2015
+ * Author: K. Benedyczak
+ */
+
+/**
+ * Tests whether the translations are complete.
+ * @author K. Benedyczak
+ */
+package pl.edu.icm.unity.test.i18n;
