2854b94a2ccaf0065bc7fc30c904000cd046e1ed
Krzysztof Benedyczak <golbi@unity-idm.eu>
2015-09-02 08:05:48 +0000

    re #256 - new package for db update tests

--- engine/src/test/java/pl/edu/icm/unity/engine/dbupdate/TestDatabaseUpdate2_1_1.java
+++ engine/src/test/java/pl/edu/icm/unity/engine/dbupdate/TestDatabaseUpdate2_1_1.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2014 ICM Uniwersytet Warszawski All rights reserved.
  * See LICENCE.txt file for licensing information.
  */
-package pl.edu.icm.unity.engine;
+package pl.edu.icm.unity.engine.dbupdate;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,65 +12,65 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import pl.edu.icm.unity.engine.internal.SessionManagementImpl;
+import pl.edu.icm.unity.engine.DBIntegrationTestBase;
 import pl.edu.icm.unity.server.api.GroupsManagement;
 import pl.edu.icm.unity.server.api.IdentitiesManagement;
 import pl.edu.icm.unity.server.api.internal.IdentityResolver;
 import pl.edu.icm.unity.server.api.internal.SessionManagement;
-import pl.edu.icm.unity.server.api.internal.TokensManagement;
+import pl.edu.icm.unity.stdext.identity.PersistentIdentity;
+import pl.edu.icm.unity.stdext.identity.TargetedPersistentIdentity;
+import pl.edu.icm.unity.stdext.identity.TransientIdentity;
 import pl.edu.icm.unity.types.basic.Entity;
 import pl.edu.icm.unity.types.basic.EntityParam;
 import pl.edu.icm.unity.types.basic.GroupContents;
 import pl.edu.icm.unity.types.basic.GroupMembership;
 import pl.edu.icm.unity.types.basic.Identity;
+import pl.edu.icm.unity.types.basic.IdentityTaV;
 
 /**
- * Warning: this test works really only after mvn clean. Otherwise it barely
- * tests anything
- * 
- * @author P. Piernik
+ * Warning: this test works really only after mvn clean. Otherwise it barely test anything
+ * @author K. Benedyczak
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:META-INF/components.xml","classpath:dbUpdate/to2_1_2/test-components.xml" })
+@ContextConfiguration(locations={"classpath*:META-INF/components.xml", "classpath:dbUpdate/to2_1_1/test-components.xml"})
 @ActiveProfiles("test")
-public class TestDatabaseUpdate2_1_2
+public class TestDatabaseUpdate2_1_1
 {
 	@Autowired
 	private SessionManagement sessionMan;
+	
 	@Autowired
-	private IdentityResolver identityResolver;
-	@Autowired
-	private TokensManagement tokensMan;
+	protected IdentityResolver identityResolver;
+	
 	@Autowired
 	protected IdentitiesManagement idsMan;
+
 	@Autowired
 	protected GroupsManagement groupsMan;
 
 	@Test
 	public void test() throws Exception
 	{
-		Assert.assertEquals(2,
-				tokensMan.getAllTokens(SessionManagementImpl.SESSION_TOKEN_TYPE)
-						.size());
-		DBIntegrationTestBase.setupUserContext(sessionMan, identityResolver, "admin", true);
+		DBIntegrationTestBase.setupUserContext(sessionMan, identityResolver, "admin", false);
 		GroupContents contents = groupsMan.getContents("/", GroupContents.MEMBERS);
-		for (GroupMembership mem : contents.getMembers())
+		for (GroupMembership mem: contents.getMembers())
 		{
 			EntityParam entityP = new EntityParam(mem.getEntityId());
 			Entity entity = idsMan.getEntityNoContext(entityP, "/");
 			Identity[] ids = entity.getIdentities();
-			Assert.assertTrue(ids.length > 1);
-			for (Identity id : ids)
+			boolean perFound = false;
+			for (Identity id: ids)
 			{
-				Assert.assertNull(id.getConfirmationInfo());
+				if (id.getTypeId().equals(PersistentIdentity.ID))
+					perFound = true;
+				if (id.getTypeId().equals(TargetedPersistentIdentity.ID) || 
+						id.getTypeId().equals(TransientIdentity.ID))
+					Assert.fail("Transient/targeted not removed");
 			}
+			if (!perFound)
+				Assert.fail("Persistent not preserved");
 		}
-		Assert.assertEquals(2,
-				tokensMan.getAllTokens(SessionManagementImpl.SESSION_TOKEN_TYPE)
-						.size());
-		Assert.assertFalse("",
-				tokensMan.getAllTokens(SessionManagementImpl.SESSION_TOKEN_TYPE)
-						.get(0).getContentsString().isEmpty());
-
+		idsMan.getEntity(new EntityParam(new IdentityTaV(PersistentIdentity.ID, 
+				"d112a078-a0b4-4ae5-8aa9-7e6744ededc5")));
 	}
 }
