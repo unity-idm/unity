631b5dce652e49d10c98e703ae5c283ef2823e92
Krzysztof Benedyczak <golbi@unity-idm.eu>
2016-02-17 22:31:29 +0000

    fixed #511 - implemented logout as an option to token revocation.

--- types/src/main/java/pl/edu/icm/unity/types/basic/EntityParam.java
+++ types/src/main/java/pl/edu/icm/unity/types/basic/EntityParam.java
@@ -58,4 +58,39 @@ public class EntityParam implements InitializationValidator
 	{
 		return entityId == null ? identity.toString() : entityId.toString();
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((entityId == null) ? 0 : entityId.hashCode());
+		result = prime * result + ((identity == null) ? 0 : identity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EntityParam other = (EntityParam) obj;
+		if (entityId == null)
+		{
+			if (other.entityId != null)
+				return false;
+		} else if (!entityId.equals(other.entityId))
+			return false;
+		if (identity == null)
+		{
+			if (other.identity != null)
+				return false;
+		} else if (!identity.equals(other.identity))
+			return false;
+		return true;
+	}
 }
