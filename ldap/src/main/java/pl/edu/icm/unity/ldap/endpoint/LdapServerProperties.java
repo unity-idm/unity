aeb4219481c80aea1f9476dbc65799c34d590eb9
Krzysztof Benedyczak <golbi@unity-idm.eu>
2016-03-22 12:48:54 +0000

    use constants for config properties

--- ldap/src/main/java/pl/edu/icm/unity/ldap/endpoint/LdapServerProperties.java
+++ ldap/src/main/java/pl/edu/icm/unity/ldap/endpoint/LdapServerProperties.java
@@ -30,19 +30,31 @@ public class LdapServerProperties extends PropertiesHelper
 	@DocumentationReferenceMeta
 	public final static Map<String, PropertyMD> META = new HashMap<>();
 	
+	public static final String HOST = "host"; 
+	public static final String LDAP_PORT = "ldapPort"; 
+	public static final String LDAPS_PORT = "ldapsPort"; 
+	public static final String BIND_PASSWORD = "bindPassword"; 
+	public static final String GROUP_QUERY = "groupQuery"; 
+	public static final String USER_QUERY = "userQuery"; 
+	public static final String GROUP_MEMBER = "groupMember"; 
+	public static final String GROUP_MEMBER_USER_REGEXP = "groupMemberUserRegexp"; 
+	public static final String RETURNED_USER_ATTRIBUTES = "returnedUserAttributes"; 
+	
 	static
 	{
 		PropertyMD.DocumentationCategory main = new PropertyMD.DocumentationCategory("General settings", "1");
-		META.put("host", new PropertyMD().setCategory(main).setDescription("LDAP server host settings"));
-		META.put("ldapPort", new PropertyMD().setCategory(main).setDescription("LDAP server port settings"));
-		META.put("ldapsPort", new PropertyMD().setCategory(main).setDescription("LDAPs server port settings"));
-		META.put("bindPassword", new PropertyMD().setCategory(main).setDescription("LDAP server bind password"));
+		META.put(HOST, new PropertyMD().setCategory(main).setDescription("LDAP server host settings"));
+		META.put(LDAP_PORT, new PropertyMD().setCategory(main).setDescription("LDAP server port settings"));
+		META.put(LDAPS_PORT, new PropertyMD().setCategory(main).setDescription("LDAPs server port settings"));
+		META.put(BIND_PASSWORD, new PropertyMD().setCategory(main).setDescription("LDAP server bind password"));
 
-		META.put("groupQuery", new PropertyMD().setCategory(main).setDescription("LDAP group query token"));
-		META.put("userQuery", new PropertyMD().setCategory(main).setDescription("LDAP user query token"));
-		META.put("groupMember", new PropertyMD().setCategory(main).setDescription("LDAP member attribute name"));
-		META.put("groupMemberUserRegexp", new PropertyMD().setCategory(main).setDescription("LDAP regexp for getting user from a member query"));
-		META.put("returnedUserAttributes", new PropertyMD().setCategory(main).setDescription("Attributes that should be returned if return all user attributes flag is set"));
+		META.put(GROUP_QUERY, new PropertyMD().setCategory(main).setDescription("LDAP group query token"));
+		META.put(USER_QUERY, new PropertyMD().setCategory(main).setDescription("LDAP user query token"));
+		META.put(GROUP_MEMBER, new PropertyMD().setCategory(main).setDescription("LDAP member attribute name"));
+		META.put(GROUP_MEMBER_USER_REGEXP, new PropertyMD().setCategory(main)
+				.setDescription("LDAP regexp for getting user from a member query"));
+		META.put(RETURNED_USER_ATTRIBUTES, new PropertyMD().setCategory(main)
+				.setDescription("Attributes that should be returned if return all user attributes flag is set"));
 	}
 	
 	public LdapServerProperties(Properties properties) throws ConfigurationException
