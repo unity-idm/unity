d79d4d7e938196fd92158f69ef7e203c62c873d6
Roman Krysinski <rkrysinski@gmail.com>
2017-02-26 16:13:37 +0100

    re #569: inject all engine api into groovy initializers scripts.

--- engine/src/main/java/pl/edu/icm/unity/engine/server/ContentGroovyExecutor.java
+++ engine/src/main/java/pl/edu/icm/unity/engine/server/ContentGroovyExecutor.java
@@ -20,7 +20,30 @@ import com.google.common.base.Stopwatch;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import pl.edu.icm.unity.base.utils.Log;
+import pl.edu.icm.unity.engine.api.AttributeClassManagement;
+import pl.edu.icm.unity.engine.api.AttributeTypeManagement;
+import pl.edu.icm.unity.engine.api.AttributesManagement;
+import pl.edu.icm.unity.engine.api.AuthenticatorManagement;
+import pl.edu.icm.unity.engine.api.BulkProcessingManagement;
+import pl.edu.icm.unity.engine.api.ConfirmationConfigurationManagement;
 import pl.edu.icm.unity.engine.api.CredentialManagement;
+import pl.edu.icm.unity.engine.api.CredentialRequirementManagement;
+import pl.edu.icm.unity.engine.api.EndpointManagement;
+import pl.edu.icm.unity.engine.api.EnquiryManagement;
+import pl.edu.icm.unity.engine.api.EntityCredentialManagement;
+import pl.edu.icm.unity.engine.api.EntityManagement;
+import pl.edu.icm.unity.engine.api.GroupsManagement;
+import pl.edu.icm.unity.engine.api.IdentityTypesManagement;
+import pl.edu.icm.unity.engine.api.InvitationManagement;
+import pl.edu.icm.unity.engine.api.MessageTemplateManagement;
+import pl.edu.icm.unity.engine.api.NotificationsManagement;
+import pl.edu.icm.unity.engine.api.PKIManagement;
+import pl.edu.icm.unity.engine.api.PreferencesManagement;
+import pl.edu.icm.unity.engine.api.RealmsManagement;
+import pl.edu.icm.unity.engine.api.RegistrationsManagement;
+import pl.edu.icm.unity.engine.api.ServerManagement;
+import pl.edu.icm.unity.engine.api.TranslationProfileManagement;
+import pl.edu.icm.unity.engine.api.UserImportManagement;
 import pl.edu.icm.unity.engine.api.config.UnityServerConfiguration;
 import pl.edu.icm.unity.engine.api.initializers.ContentInitConf;
 import pl.edu.icm.unity.engine.api.initializers.InitializerType;
@@ -39,10 +62,74 @@ public class ContentGroovyExecutor
 	private GroovyShell shell;
 	
 	@Autowired
-	@Qualifier("insecure")
-	private CredentialManagement credMan;
-	@Autowired
 	private UnityServerConfiguration config;
+	@Autowired
+	private BulkProcessingManagement bulkProcessingManagement;
+	@Autowired
+	private IdentityTypesManagement identityTypesManagement;
+	@Autowired
+	private PKIManagement pkiManagement;
+	@Autowired
+	private PreferencesManagement preferencesManagement;
+	@Autowired
+	private UserImportManagement userImportManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private AttributeClassManagement attributeClassManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private AttributesManagement attributesManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private AttributeTypeManagement attributeTypeManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private AuthenticatorManagement authenticatorManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private ConfirmationConfigurationManagement confirmationConfigurationManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private CredentialManagement credentialManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private CredentialRequirementManagement credentialRequirementManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private EndpointManagement endpointManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private EnquiryManagement enquiryManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private EntityCredentialManagement entityCredentialManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private EntityManagement entityManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private GroupsManagement groupsManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private InvitationManagement invitationManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private MessageTemplateManagement messageTemplateManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private NotificationsManagement notificationsManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private RealmsManagement realmsManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private RegistrationsManagement registrationsManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private ServerManagement serverManagement;
+	@Autowired
+	@Qualifier("insecure")
+	private TranslationProfileManagement translationProfileManagement;
 
 	@PostConstruct
 	public void initialize()
@@ -76,7 +163,31 @@ public class ContentGroovyExecutor
 	private Binding getBinding()
 	{
 		Binding binding = new Binding();
-		binding.setVariable("credentialManagement", credMan);
+		binding.setVariable("config", config);
+		binding.setVariable("attributeClassManagement", attributeClassManagement);
+		binding.setVariable("attributesManagement", attributesManagement);
+		binding.setVariable("attributeTypeManagement", attributeTypeManagement);
+		binding.setVariable("authenticatorManagement", authenticatorManagement);
+		binding.setVariable("bulkProcessingManagement", bulkProcessingManagement);
+		binding.setVariable("confirmationConfigurationManagement", confirmationConfigurationManagement);
+		binding.setVariable("credentialManagement", credentialManagement);
+		binding.setVariable("credentialRequirementManagement", credentialRequirementManagement);
+		binding.setVariable("endpointManagement", endpointManagement);
+		binding.setVariable("enquiryManagement", enquiryManagement);
+		binding.setVariable("entityCredentialManagement", entityCredentialManagement);
+		binding.setVariable("entityManagement", entityManagement);
+		binding.setVariable("groupsManagement", groupsManagement);
+		binding.setVariable("identityTypesManagement", identityTypesManagement);
+		binding.setVariable("invitationManagement", invitationManagement);
+		binding.setVariable("messageTemplateManagement", messageTemplateManagement);
+		binding.setVariable("notificationsManagement", notificationsManagement);
+		binding.setVariable("pkiManagement", pkiManagement);
+		binding.setVariable("preferencesManagement", preferencesManagement);
+		binding.setVariable("realmsManagement", realmsManagement);
+		binding.setVariable("registrationsManagement", registrationsManagement);
+		binding.setVariable("serverManagement", serverManagement);
+		binding.setVariable("translationProfileManagement", translationProfileManagement);
+		binding.setVariable("userImportManagement", userImportManagement);
 		binding.setVariable("config", config);
 		binding.setVariable("log", LOG);
 		return binding;
