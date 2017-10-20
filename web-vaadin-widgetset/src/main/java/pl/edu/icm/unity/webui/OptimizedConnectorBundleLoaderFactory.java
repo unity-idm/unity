3d087be5719b55337ddc8288eb9906a6dd656dee
Roman Krysinski <rkrysinski@gmail.com>
2017-10-20 18:30:08 +0200

    UY-579: cleaning commpilation errors for unity-server-web-vaadin-widgetset, RpcDataSourceConnector not supported (not sure yet if this has any side effects, possibly not since stepper version was bumped to the one which supports vaadin8)

--- web-vaadin-widgetset/src/main/java/pl/edu/icm/unity/webui/OptimizedConnectorBundleLoaderFactory.java
+++ web-vaadin-widgetset/src/main/java/pl/edu/icm/unity/webui/OptimizedConnectorBundleLoaderFactory.java
@@ -8,13 +8,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.google.gwt.core.ext.typeinfo.JClassType;
-import com.vaadin.v7.client.ui.checkbox.CheckBoxConnector;
+import com.vaadin.client.ui.checkbox.CheckBoxConnector;
 import com.vaadin.server.widgetsetutils.ConnectorBundleLoaderFactory;
 import com.vaadin.shared.ui.Connect.LoadStyle;
 
 public class OptimizedConnectorBundleLoaderFactory extends ConnectorBundleLoaderFactory {
 
-	private Set<String> eagerConnectors = new HashSet<String>();
+	private Set<String> eagerConnectors = new HashSet<>();
 	{
 		eagerConnectors.add(com.vaadin.client.ui.ui.UIConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.combobox.ComboBoxConnector.class.getName());
@@ -23,14 +23,13 @@ public class OptimizedConnectorBundleLoaderFactory extends ConnectorBundleLoader
 		eagerConnectors.add(com.vaadin.client.ui.passwordfield.PasswordFieldConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.gridlayout.GridLayoutConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.label.LabelConnector.class.getName());
-		eagerConnectors.add(com.vaadin.client.connectors.GridConnector.class.getName());
+		eagerConnectors.add(com.vaadin.client.connectors.grid.GridConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.button.ButtonConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.customcomponent.CustomComponentConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.orderedlayout.HorizontalLayoutConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.orderedlayout.VerticalLayoutConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.formlayout.FormLayoutConnector.class.getName());
-		eagerConnectors.add(com.vaadin.client.connectors.RpcDataSourceConnector.class.getName());
-		eagerConnectors.add(com.vaadin.client.connectors.TextRendererConnector.class.getName());
+		eagerConnectors.add(com.vaadin.client.connectors.grid.TextRendererConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.ui.csslayout.CssLayoutConnector.class.getName());
 		eagerConnectors.add(com.vaadin.client.connectors.ImageRendererConnector.class.getName());
 		eagerConnectors.add(CheckBoxConnector.class.getName());
