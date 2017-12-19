c8a6d2016fcbc770762bbd0432de5ebb087f8288
Krzysztof Benedyczak <golbi@unity-idm.eu>
2017-12-19 21:02:01 +0000

    UY-661 fallback to default syntax on dynamic attributes
    Add tests and use proper packages in tests.

--- saml/src/test/java/pl/edu/icm/unity/saml/idp/TestSAMLBindingUtils.java
+++ saml/src/test/java/pl/edu/icm/unity/saml/idp/TestSAMLBindingUtils.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2014 ICM Uniwersytet Warszawski All rights reserved.
  * See LICENCE.txt file for licensing information.
  */
-package pl.edu.icm.unity.samlidp;
+package pl.edu.icm.unity.saml.idp;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
