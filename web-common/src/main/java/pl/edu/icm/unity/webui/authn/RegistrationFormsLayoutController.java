50408271cdfc3eb3b55983bef84d6d196a4ef4ce
Krzysztof Benedyczak <golbi@unity-idm.eu>
2018-10-18 00:01:31 +0200

    Remove wrong Jirav copyright notices - generated by mistake, wrong IDE config.

--- web-common/src/main/java/pl/edu/icm/unity/webui/authn/RegistrationFormsLayoutController.java
+++ web-common/src/main/java/pl/edu/icm/unity/webui/authn/RegistrationFormsLayoutController.java
@@ -1,11 +1,7 @@
-/**********************************************************************
- *                     Copyright (c) 2018, Jirav
- *                        All Rights Reserved
- *
- *         This is unpublished proprietary source code of Jirav.
- *    Reproduction or distribution, in whole or in part, is forbidden
- *          except by express written permission of Jirav, Inc.
- **********************************************************************/
+/*
+ * Copyright (c) 2018 Bixbit - Krzysztof Benedyczak All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
 package pl.edu.icm.unity.webui.authn;
 
 import java.util.List;
