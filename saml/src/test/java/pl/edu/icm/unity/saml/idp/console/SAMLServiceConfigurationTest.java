243ee31016017adb5c45ff1edad43ca49c354511
Krzysztof Benedyczak <golbi@unity-idm.eu>
2019-11-30 21:57:10 +0100

    UY-963 add tests of SAML web idp editor in console.
    Moved related conf generation test framework from oauth to engine.

---
+++ saml/src/test/java/pl/edu/icm/unity/saml/idp/console/SAMLServiceConfigurationTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2019 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+package pl.edu.icm.unity.saml.idp.console;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static pl.edu.icm.unity.configtester.ConfigurationComparator.createComparator;
+import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.P;
+import static pl.edu.icm.unity.saml.idp.SamlIdpProperties.defaults;
+
+import java.security.cert.X509Certificate;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import pl.edu.icm.unity.configtester.ConfigurationComparator;
+import pl.edu.icm.unity.configtester.ConfigurationGenerator;
+import pl.edu.icm.unity.engine.api.PKIManagement;
+import pl.edu.icm.unity.engine.api.files.FileStorageService;
+import pl.edu.icm.unity.engine.api.files.URIAccessService;
+import pl.edu.icm.unity.engine.api.msg.UnityMessageSource;
+import pl.edu.icm.unity.engine.api.pki.NamedCertificate;
+import pl.edu.icm.unity.types.translation.ProfileType;
+import pl.edu.icm.unity.types.translation.TranslationProfile;
+
+public class SAMLServiceConfigurationTest
+{
+	private PKIManagement pkiMan = mock(PKIManagement.class);
+	private UnityMessageSource msg = mock(UnityMessageSource.class);
+	private URIAccessService uriAccessSrv = mock(URIAccessService.class);
+	private FileStorageService fileStorageSrv = mock(FileStorageService.class);
+	
+	@Test
+	public void serializationIsIdempotentForMinimalConfig() throws Exception
+	{
+		when(pkiMan.getCredentialNames()).thenReturn(Sets.newHashSet("foo"));
+		NamedCertificate nc = new NamedCertificate("foo", mock(X509Certificate.class));
+		when(pkiMan.getCertificate(any())).thenReturn(nc);
+		TranslationProfile tp = new TranslationProfile("name", "description", ProfileType.OUTPUT, Collections.emptyList());
+		Properties sourceCfg = ConfigurationGenerator.generateMinimalDefault(P, defaults)
+				.update("embeddedTranslationProfile", tp.toJsonObject().toString())
+				.remove("acceptedSP.1.encryptAssertion")
+				.update("defaultGroup", "/foo1")
+				.update("groupMapping.1.mappingGroup", "/foo2")
+				.get();
+		SAMLServiceConfiguration processor = new SAMLServiceConfiguration(Collections.emptyList());
+		
+		processor.fromProperties(ConfigurationComparator.getAsString(sourceCfg), msg, uriAccessSrv, pkiMan, Lists.newArrayList());
+		String converted = processor.toProperties(pkiMan, msg, fileStorageSrv, "name");
+		
+		Properties result = ConfigurationComparator.fromString(converted, P).get();
+		
+		createComparator(P, defaults)
+				.ignoringMissing("translationProfile", "")
+				.ignoringSuperflous("acceptedSPMetadataSource.1.refreshInterval", 
+						"acceptedSPMetadataSource.1.signaturVerification")
+				.checkMatching(result, sourceCfg);
+	}
+	
+	@Test
+	public void serializationIsIdempotentForMinimalExplicitDefaultsConfig() throws Exception
+	{
+		when(pkiMan.getCredentialNames()).thenReturn(Sets.newHashSet("foo"));
+		NamedCertificate nc = new NamedCertificate("foo", mock(X509Certificate.class));
+		when(pkiMan.getCertificate(any())).thenReturn(nc);
+		TranslationProfile tp = new TranslationProfile("name", "description", ProfileType.OUTPUT, Collections.emptyList());
+		Properties sourceCfg = ConfigurationGenerator.generateMinimalWithDefaults(P, defaults)
+				.update("embeddedTranslationProfile", tp.toJsonObject().toString())
+				.remove("acceptedSP.1.encryptAssertion")
+				.update("defaultGroup", "/foo1")
+				.update("groupMapping.1.mappingGroup", "/foo2")
+				.get();
+		SAMLServiceConfiguration processor = new SAMLServiceConfiguration(Collections.emptyList());
+		
+		processor.fromProperties(ConfigurationComparator.getAsString(sourceCfg), msg, uriAccessSrv, pkiMan, Lists.newArrayList());
+		String converted = processor.toProperties(pkiMan, msg, fileStorageSrv, "name");
+		
+		Properties result = ConfigurationComparator.fromString(converted, P).get();
+		
+		createComparator(P, defaults)
+				.ignoringMissing("translationProfile", "")
+				.checkMatching(result, sourceCfg);
+	}
+	
+	@Test
+	public void serializationIsIdempotentForCompleteNonDefaultConfig() throws Exception
+	{
+		when(pkiMan.getCredentialNames()).thenReturn(Sets.newHashSet("foo"));
+		NamedCertificate nc = new NamedCertificate("foo", mock(X509Certificate.class));
+		when(pkiMan.getCertificate(any())).thenReturn(nc);
+		TranslationProfile tp = new TranslationProfile("name", "description", ProfileType.OUTPUT, Collections.emptyList());
+		Properties sourceCfg = ConfigurationGenerator.generateCompleteWithNonDefaults(P, defaults)
+				.update("embeddedTranslationProfile", tp.toJsonObject().toString())
+				.update("acceptedSP.1.returnURLs.1", "[0]https://foo")
+				.remove("acceptedSP.1.dn")
+				.update("defaultGroup", "/foo1")
+				.update("groupMapping.1.mappingGroup", "/foo2")
+				.get();
+		SAMLServiceConfiguration processor = new SAMLServiceConfiguration(Collections.emptyList());
+		
+		processor.fromProperties(ConfigurationComparator.getAsString(sourceCfg), msg, uriAccessSrv, pkiMan, Lists.newArrayList());
+		String converted = processor.toProperties(pkiMan, msg, fileStorageSrv, "name");
+		
+		Properties result = ConfigurationComparator.fromString(converted, P).get();
+		
+		createComparator(P, defaults)
+				.ignoringMissing("translationProfile", "")
+				.withAlias("unity.saml.acceptedSP.1.certificates.2", "unity.saml.acceptedSP.1.certificates.1")
+				.withAlias("unity.saml.acceptedSP.1.certificates.1", "unity.saml.acceptedSP.1.certificate")
+				.withAlias("unity.saml.acceptedSP.1.returnURLs.2", "unity.saml.acceptedSP.1.returnURLs.1")
+				.checkMatching(result, sourceCfg);
+	}
+}
