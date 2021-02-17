ae28a5c17c687221075114f066b9da3defae7978
Krzysztof Benedyczak <golbi@unity-idm.eu>
2021-02-17 18:37:29 +0100

    UY-1126 fix searching by email used in upman

---
+++ engine/src/main/java/pl/edu/icm/unity/engine/project/ExistingUserFinder.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2021 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+package pl.edu.icm.unity.engine.project;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import pl.edu.icm.unity.engine.api.bulk.BulkGroupQueryService;
+import pl.edu.icm.unity.engine.api.bulk.EntityInGroupData;
+import pl.edu.icm.unity.engine.api.bulk.GroupMembershipData;
+import pl.edu.icm.unity.exceptions.EngineException;
+import pl.edu.icm.unity.stdext.identity.EmailIdentity;
+import pl.edu.icm.unity.stdext.utils.ContactEmailMetadataProvider;
+import pl.edu.icm.unity.types.basic.Attribute;
+import pl.edu.icm.unity.types.basic.Identity;
+import pl.edu.icm.unity.types.basic.VerifiableElementBase;
+import pl.edu.icm.unity.types.basic.VerifiableEmail;
+
+
+@Component
+class ExistingUserFinder
+{
+	private final BulkGroupQueryService bulkService;
+	private final ProjectAttributeHelper attrHelper;
+	
+	@Autowired
+	ExistingUserFinder(@Qualifier("insecure") BulkGroupQueryService bulkService, ProjectAttributeHelper attrHelper)
+	{
+		this.bulkService = bulkService;
+		this.attrHelper = attrHelper;
+	}
+
+	Long getEntityIdByContactAddress(String contactAddress) throws EngineException
+	{
+		GroupMembershipData bulkMembershipData = bulkService.getBulkMembershipData("/");
+		Map<Long, EntityInGroupData> members = bulkService.getMembershipInfo(bulkMembershipData);
+
+		VerifiableEmail searchedEmail = new VerifiableEmail(contactAddress);
+		String searchedComparable = searchedEmail.getComparableValue();
+		
+		for (EntityInGroupData info : members.values())
+		{
+			Identity emailId = info.entity.getIdentities().stream()
+					.filter(id -> id.getTypeId().equals(EmailIdentity.ID))
+					.filter(id -> emailsEqual(searchedComparable, id))
+					.findAny().orElse(null);
+			if (emailId != null)
+				return info.entity.getId();
+		}
+
+		return searchEntityByEmailAttr(members, searchedComparable);
+	}
+
+	private boolean emailsEqual(String comparableEmail1, Identity emailIdentity)
+	{
+		VerifiableEmail verifiableEmail = EmailIdentity.fromIdentityParam(emailIdentity);
+		return comparableEmail1.equals(verifiableEmail.getComparableValue());
+	}
+	
+	private Long searchEntityByEmailAttr(Map<Long, EntityInGroupData> membersWithGroups, String comparableContactAddress)
+			throws EngineException
+	{
+		for (EntityInGroupData info : membersWithGroups.values())
+		{
+			VerifiableElementBase contactEmail = attrHelper.searchVerifiableAttributeValueByMeta(ContactEmailMetadataProvider.NAME,
+					info.groupAttributesByName.values().stream().map(e -> (Attribute) e)
+							.collect(Collectors.toList()));
+			if (contactEmail != null && contactEmail.getValue() != null)
+			{
+				VerifiableEmail verifiableEmail = (VerifiableEmail)contactEmail;
+				if (verifiableEmail.getComparableValue().equals(comparableContactAddress))
+					return info.entity.getId();
+			}
+		}
+
+		return null;
+	}	
+}
