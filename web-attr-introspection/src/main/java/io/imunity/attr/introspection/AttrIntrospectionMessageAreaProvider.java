a2c1992008e518d8074974953b344da5b8903d3d
Piotr Piernik <piotrpiernik@gmail.com>
2022-01-25 08:19:38 +0100

    Add attribute introspection message area provider

---
+++ web-attr-introspection/src/main/java/io/imunity/attr/introspection/AttrIntrospectionMessageAreaProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+
+package io.imunity.attr.introspection;
+
+import org.springframework.stereotype.Component;
+
+import pl.edu.icm.unity.MessageArea;
+import pl.edu.icm.unity.msg.MessageAreaProvider;
+
+@Component
+public class AttrIntrospectionMessageAreaProvider implements MessageAreaProvider
+{
+	public final String NAME = "attr-introspection";
+
+	@Override
+	public MessageArea getMessageArea()
+	{
+		return new MessageArea(NAME, "AttrIntrospectionMessageAreaProvider.displayedName", false);
+	}
+
+	@Override
+	public String getName()
+	{
+		return NAME;
+	}
+}
