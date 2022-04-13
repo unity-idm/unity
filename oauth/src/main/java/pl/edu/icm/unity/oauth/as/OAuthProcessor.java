ec91e06972a0519fc50d12a8397013339cea055f
Piotr Piernik <piotrpiernik@gmail.com>
2022-04-13 19:21:51 +0200

    UY-1229 revert invalid default Audience -> clientName to clientUserName

--- oauth/src/main/java/pl/edu/icm/unity/oauth/as/OAuthProcessor.java
+++ oauth/src/main/java/pl/edu/icm/unity/oauth/as/OAuthProcessor.java
@@ -108,7 +108,7 @@ public class OAuthProcessor
 		internalToken.setSubject(identity.getValue());
 		internalToken.setMaxExtendedValidity(config.getMaxExtendedAccessTokenValidity());
 		internalToken.setTokenValidity(config.getAccessTokenValidity()); 
-		internalToken.setAudience(Stream.concat(Stream.of(ctx.getClientName()), ctx.getAdditionalAudience().stream()).collect(Collectors.toList()));
+		internalToken.setAudience(Stream.concat(Stream.of(ctx.getClientUsername()), ctx.getAdditionalAudience().stream()).collect(Collectors.toList()));
 		internalToken.setIssuerUri(config.getIssuerName());
 		internalToken.setClientType(ctx.getClientType());
 		
@@ -279,7 +279,7 @@ public class OAuthProcessor
 		IDTokenClaimsSet idToken = new IDTokenClaimsSet(
 				new Issuer(context.getConfig().getIssuerName()), 
 				new Subject(userIdentity), 
-				audience.stream().map(Audience::new).collect(Collectors.toList()), 
+				audience.stream().filter(a -> a != null).map(Audience::new).collect(Collectors.toList()), 
 				new Date(now.getTime() + context.getConfig().getIdTokenValidity()*1000), 
 				now);
 		ResponseType responseType = request.getResponseType();
