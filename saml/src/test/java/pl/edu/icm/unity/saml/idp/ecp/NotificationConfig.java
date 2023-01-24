769683d27effa067ca63109a76b2bb6f0d66dbe8
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2023-01-24 12:39:17 +0100

    UY-1304 tests environment fix

---
+++ saml/src/test/java/pl/edu/icm/unity/saml/idp/ecp/NotificationConfig.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+
+package pl.edu.icm.unity.saml.idp.ecp;
+
+import io.imunity.vaadin23.elements.NotificationPresenter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class NotificationConfig
+{
+	@Bean
+	public NotificationPresenter create()
+	{
+		return new NotificationPresenter();
+	}
+}
