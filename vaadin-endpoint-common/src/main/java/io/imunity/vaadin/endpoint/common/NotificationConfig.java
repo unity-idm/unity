1ea7a257bb1a41194d82748b232ecfa03d882073
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2024-01-05 16:13:44 +0100

    UY-1379 Authenticators - fixed tests

--- vaadin-endpoint-common/src/main/java/io/imunity/vaadin/endpoint/common/NotificationConfig.java
+++ vaadin-endpoint-common/src/main/java/io/imunity/vaadin/endpoint/common/NotificationConfig.java
@@ -1,18 +1,20 @@
 /*
- * Copyright (c) 2018 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * Copyright (c) 2021 Bixbit - Krzysztof Benedyczak. All rights reserved.
  * See LICENCE.txt file for licensing information.
  */
 
-package io.imunity.upman.front;
+package io.imunity.vaadin.endpoint.common;
 
 import io.imunity.vaadin.elements.NotificationPresenter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 class NotificationConfig
 {
 	@Bean
+	@Primary
 	public NotificationPresenter create()
 	{
 		return new NotificationPresenter();
