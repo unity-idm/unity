863a44f2061540b9b027ff94ad067927740523fc
Piotr Piernik <piotrpiernik@gmail.com>
2024-03-27 14:26:41 +0100

    update credentials view to use GridWithAction

--- console/src/main/java/io/imunity/console/views/authentication/credentials/CredentialsView.java
+++ console/src/main/java/io/imunity/console/views/authentication/credentials/CredentialsView.java
@@ -5,35 +5,33 @@
 
 package io.imunity.console.views.authentication.credentials;
 
+import static io.imunity.console.views.ViewHeaderActionLayoutFactory.createHeaderActionLayout;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 import com.google.common.collect.Sets;
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
-import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridSortOrder;
 import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
+
 import io.imunity.console.ConsoleMenu;
 import io.imunity.console.views.ConsoleViewComponent;
-import io.imunity.vaadin.elements.ActionIconBuilder;
 import io.imunity.vaadin.elements.Breadcrumb;
+import io.imunity.vaadin.elements.grid.GridWithActionColumn;
+import io.imunity.vaadin.elements.grid.SingleActionHandler;
 import io.imunity.vaadin.endpoint.common.WebSession;
 import io.imunity.vaadin.endpoint.common.bus.EventsBus;
 import jakarta.annotation.security.PermitAll;
 import pl.edu.icm.unity.base.authn.CredentialDefinition;
 import pl.edu.icm.unity.base.message.MessageSource;
 import pl.edu.icm.unity.engine.api.utils.MessageUtils;
-
-import java.util.Comparator;
-
-import static com.vaadin.flow.component.icon.VaadinIcon.*;
-import static io.imunity.console.views.ViewHeaderActionLayoutFactory.createHeaderActionLayout;
-
 
 @PermitAll
 @Breadcrumb(key = "WebConsoleMenu.authentication.localCredentials", parent = "WebConsoleMenu.authentication")
@@ -43,13 +41,14 @@ public class CredentialsView extends ConsoleViewComponent
 	private final CredentialsController controller;
 	private final MessageSource msg;
 	private final EventsBus bus;
-	private Grid<CredentialDefinition> credList;
+	private GridWithActionColumn<CredentialDefinition> credList;
 
 	CredentialsView(MessageSource msg, CredentialsController controller)
 	{
 		this.controller = controller;
 		this.msg = msg;
-		this.bus = WebSession.getCurrent().getEventBus();
+		this.bus = WebSession.getCurrent()
+				.getEventBus();
 		init();
 	}
 
@@ -57,71 +56,74 @@ public class CredentialsView extends ConsoleViewComponent
 	{
 		VerticalLayout buttonsBar = createHeaderActionLayout(msg, CredentialsEditView.class);
 
-		credList = new Grid<>();
+		credList = new GridWithActionColumn<>(msg::getMessage, getActionsHandlers());
 		credList.addThemeVariants(GridVariant.LUMO_NO_BORDER);
 		Grid.Column<CredentialDefinition> nameColumn = credList.addComponentColumn(entry ->
-				{
-					if (!entry.isReadOnly())
-						return new RouterLink(entry.getName(), CredentialsEditView.class, entry.getName());
-					else
-						return new RouterLink(entry.getName(), CredentialsInfoView.class, entry.getName());
-				})
+		{
+			if (!entry.isReadOnly())
+				return new RouterLink(entry.getName(), CredentialsEditView.class, entry.getName());
+			else
+				return new RouterLink(entry.getName(), CredentialsInfoView.class, entry.getName());
+		})
 				.setHeader(msg.getMessage("LocalCredentialsView.nameCaption"))
 				.setSortable(true)
 				.setComparator(Comparator.comparing(CredentialDefinition::getName));
-
-		credList.addComponentColumn(this::createRowActionMenu)
-				.setHeader(msg.getMessage("actions"))
-				.setTextAlign(ColumnTextAlign.END);
-
 		credList.setItems(controller.getCredentials());
-		credList.sort(GridSortOrder.asc(nameColumn).build());
+		credList.sort(GridSortOrder.asc(nameColumn)
+				.build());
 
 		VerticalLayout main = new VerticalLayout();
 		main.add(buttonsBar, credList);
 		getContent().add(main);
 	}
 
-	private Component createRowActionMenu(CredentialDefinition credentialDefinition)
+	private List<SingleActionHandler<CredentialDefinition>> getActionsHandlers()
 	{
-		Icon generalSettings = new ActionIconBuilder()
-				.icon(EDIT)
-				.tooltipText(msg.getMessage("edit"))
-				.navigation(CredentialsEditView.class, credentialDefinition.getName())
-				.build();
-		Icon remove = new ActionIconBuilder()
-				.icon(TRASH)
-				.tooltipText(msg.getMessage("remove"))
-				.clickListener(() -> tryRemove(credentialDefinition))
-				.build();
-		Icon info = new ActionIconBuilder()
-				.icon(INFO)
-				.tooltipText(msg.getMessage("showDetails"))
-				.navigation(CredentialsInfoView.class, credentialDefinition.getName())
+		SingleActionHandler<CredentialDefinition> show = SingleActionHandler
+				.builder4ShowDetails(msg::getMessage, CredentialDefinition.class)
+				.withHandler(r -> gotoShowDetails(r.iterator()
+						.next()))
+				.withDisabledPredicate(r -> !r.isReadOnly())
+				.hideIfInactive()
 				.build();
 
-		HorizontalLayout horizontalLayout = new HorizontalLayout();
-		horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+		SingleActionHandler<CredentialDefinition> edit = SingleActionHandler
+				.builder4Edit(msg::getMessage, CredentialDefinition.class)
+				.withHandler(r -> gotoEdit(r.iterator()
+						.next()))
+				.withDisabledPredicate(r -> r.isReadOnly())
+				.hideIfInactive()
+				.build();
 
-		if(credentialDefinition.isReadOnly())
-			horizontalLayout.add(info, remove);
-		else
-			horizontalLayout.add(generalSettings, remove);
+		SingleActionHandler<CredentialDefinition> remove = SingleActionHandler
+				.builder4Delete(msg::getMessage, CredentialDefinition.class)
+				.withHandler(r -> tryRemove(r.iterator()
+						.next()))
+				.build();
 
-		return horizontalLayout;
+		return Arrays.asList(show, edit, remove);
+	}
+
+	private void gotoShowDetails(CredentialDefinition cred)
+	{
+		UI.getCurrent()
+				.navigate(CredentialsInfoView.class, cred.getName());
+	}
+
+	private void gotoEdit(CredentialDefinition cred)
+	{
+		UI.getCurrent()
+				.navigate(CredentialsEditView.class, cred.getName());
 	}
 
 	private void tryRemove(CredentialDefinition cred)
 	{
 		String confirmText = MessageUtils.createConfirmFromStrings(msg, Sets.newHashSet(cred.getName()));
-		new ConfirmDialog(
-				msg.getMessage("ConfirmDialog.confirm"),
-				msg.getMessage("LocalCredentialsView.confirmDelete", confirmText),
-				msg.getMessage("ok"),
-				e -> remove(cred),
-				msg.getMessage("cancel"),
-				e -> {}
-		).open();
+		new ConfirmDialog(msg.getMessage("ConfirmDialog.confirm"),
+				msg.getMessage("LocalCredentialsView.confirmDelete", confirmText), msg.getMessage("ok"),
+				e -> remove(cred), msg.getMessage("cancel"), e ->
+				{
+				}).open();
 	}
 
 	private void remove(CredentialDefinition cred)
