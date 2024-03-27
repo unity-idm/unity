4296b69e3fc805c9d5175b0801c23cf3f57c4d4e
Piotr Piernik <piotrpiernik@gmail.com>
2024-03-27 12:26:59 +0100

    UY-1372 update attr classes view to use GridWithAction

--- console/src/main/java/io/imunity/console/views/directory_setup/attribute_classes/AttributeClassesView.java
+++ console/src/main/java/io/imunity/console/views/directory_setup/attribute_classes/AttributeClassesView.java
@@ -5,41 +5,37 @@
 
 package io.imunity.console.views.directory_setup.attribute_classes;
 
+import static io.imunity.console.views.ViewHeaderActionLayoutFactory.createHeaderActionLayout;
+import static io.imunity.vaadin.elements.CssClassNames.GRID_DETAILS_FORM;
+import static io.imunity.vaadin.elements.CssClassNames.GRID_DETAILS_FORM_ITEM;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
 import com.google.common.collect.Sets;
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridSortOrder;
-import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
+
 import io.imunity.console.ConsoleMenu;
 import io.imunity.console.views.ConsoleViewComponent;
-import io.imunity.vaadin.elements.ActionIconBuilder;
 import io.imunity.vaadin.elements.Breadcrumb;
-
+import io.imunity.vaadin.elements.grid.GridWithActionColumn;
+import io.imunity.vaadin.elements.grid.SingleActionHandler;
 import jakarta.annotation.security.PermitAll;
 import pl.edu.icm.unity.base.attribute.AttributesClass;
 import pl.edu.icm.unity.base.describedObject.DescribedObjectROImpl;
 import pl.edu.icm.unity.base.message.MessageSource;
 import pl.edu.icm.unity.engine.api.utils.MessageUtils;
-
-import java.util.Comparator;
-
-import static com.vaadin.flow.component.icon.VaadinIcon.EDIT;
-import static com.vaadin.flow.component.icon.VaadinIcon.TRASH;
-import static io.imunity.console.views.ViewHeaderActionLayoutFactory.createHeaderActionLayout;
-import static io.imunity.vaadin.elements.CssClassNames.GRID_DETAILS_FORM;
-import static io.imunity.vaadin.elements.CssClassNames.GRID_DETAILS_FORM_ITEM;
 
 @PermitAll
 @Breadcrumb(key = "WebConsoleMenu.directorySetup.attributeClasses", parent = "WebConsoleMenu.directorySetup")
@@ -48,7 +44,7 @@ public class AttributeClassesView extends ConsoleViewComponent
 {
 	private final MessageSource msg;
 	private final AttributeClassController controller;
-	private Grid<AttributesClass> attributesClassGrid;
+	private GridWithActionColumn<AttributesClass> attributesClassGrid;
 
 	AttributeClassesView(MessageSource msg, AttributeClassController controller)
 	{
@@ -59,11 +55,12 @@ public class AttributeClassesView extends ConsoleViewComponent
 
 	public void initUI()
 	{
-		attributesClassGrid = new Grid<>();
-		attributesClassGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
-		attributesClassGrid.setItemDetailsRenderer(new ComponentRenderer<>(this::getDetailsComponent));
 
-		Grid.Column<AttributesClass> nameColumn = attributesClassGrid.addComponentColumn(this::createNameWithDetailsArrow)
+		attributesClassGrid = new GridWithActionColumn<AttributesClass>(msg::getMessage, getActionsHandlers());
+		attributesClassGrid.addShowDetailsColumn(new ComponentRenderer<>(this::getDetailsComponent));
+
+		Grid.Column<AttributesClass> nameColumn = attributesClassGrid
+				.addComponentColumn(this::createNameWithDetailsArrow)
 				.setHeader(msg.getMessage("AttributeClassesView.nameCaption"))
 				.setAutoWidth(true)
 				.setSortable(true)
@@ -76,48 +73,52 @@ public class AttributeClassesView extends ConsoleViewComponent
 				.setHeader(msg.getMessage("AttributeClassesView.mandatoryCaption"))
 				.setAutoWidth(true)
 				.setSortable(true);
-		attributesClassGrid.addComponentColumn(this::createRowActionMenu)
-				.setHeader(msg.getMessage("actions"))
-				.setTextAlign(ColumnTextAlign.END);
+//		attributesClassGrid.addComponentColumn(this::createRowActionMenu)
+//				.setHeader(msg.getMessage("actions"))
+//				.setTextAlign(ColumnTextAlign.END);
 
-		attributesClassGrid.sort(GridSortOrder.asc(nameColumn).build());
+		attributesClassGrid.sort(GridSortOrder.asc(nameColumn)
+				.build());
 		attributesClassGrid.setItems(controller.getAttributeClasses());
 
-		VerticalLayout main = new VerticalLayout(createHeaderActionLayout(msg, AttributeClassesEditView.class), attributesClassGrid);
+		VerticalLayout main = new VerticalLayout(createHeaderActionLayout(msg, AttributeClassesEditView.class),
+				attributesClassGrid);
 		main.setSpacing(false);
 		getContent().add(main);
 	}
 
-	private Component createRowActionMenu(AttributesClass attributesClass)
+	private List<SingleActionHandler<AttributesClass>> getActionsHandlers()
 	{
-		Icon generalSettings = new ActionIconBuilder()
-				.icon(EDIT)
-				.tooltipText(msg.getMessage("edit"))
-				.navigation(AttributeClassesEditView.class, attributesClass.getName())
+		SingleActionHandler<AttributesClass> edit = SingleActionHandler
+				.builder4Edit(msg::getMessage, AttributesClass.class)
+				.withHandler(r -> gotoEdit(r.iterator()
+						.next()))
 				.build();
 
-		Icon remove = new ActionIconBuilder()
-				.icon(TRASH)
-				.tooltipText(msg.getMessage("remove"))
-				.clickListener(() -> tryRemove(attributesClass))
+		SingleActionHandler<AttributesClass> remove = SingleActionHandler
+				.builder4Delete(msg::getMessage, AttributesClass.class)
+				.withHandler(this::tryRemove)
 				.build();
 
-		HorizontalLayout horizontalLayout = new HorizontalLayout(generalSettings, remove);
-		horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-		return horizontalLayout;
+		return Arrays.asList(edit, remove);
 	}
 
-	private void tryRemove(AttributesClass attributesClass)
+	private void gotoEdit(AttributesClass next)
 	{
-		String confirmText = MessageUtils.createConfirmFromStrings(msg, Sets.newHashSet(attributesClass.getName()));
-		new ConfirmDialog(
-				msg.getMessage("ConfirmDialog.confirm"),
-				msg.getMessage("AttributeClassesView.confirmDelete", confirmText),
-				msg.getMessage("ok"),
-				e -> remove(attributesClass),
-				msg.getMessage("cancel"),
-				e -> {}
-		).open();
+		UI.getCurrent()
+				.navigate(AttributeClassesEditView.class, next.getName());
+	}
+
+	private void tryRemove(Set<AttributesClass> items)
+	{
+		AttributesClass item = items.iterator()
+				.next();
+		String confirmText = MessageUtils.createConfirmFromStrings(msg, Sets.newHashSet(item.getName()));
+		new ConfirmDialog(msg.getMessage("ConfirmDialog.confirm"),
+				msg.getMessage("AttributeClassesView.confirmDelete", confirmText), msg.getMessage("ok"),
+				e -> remove(item), msg.getMessage("cancel"), e ->
+				{
+				}).open();
 	}
 
 	private void remove(AttributesClass attrClass)
@@ -129,25 +130,16 @@ public class AttributeClassesView extends ConsoleViewComponent
 	private FormLayout getDetailsComponent(AttributesClass attributesClass)
 	{
 		FormLayout wrapper = new FormLayout();
-		FormLayout.FormItem formItem = wrapper.addFormItem(
-				new Span(attributesClass.getDescription()),
-				msg.getMessage("AttributeClassesView.descriptionLabelCaption")
-		);
+		FormLayout.FormItem formItem = wrapper.addFormItem(new Span(attributesClass.getDescription()),
+				msg.getMessage("AttributeClassesView.descriptionLabelCaption"));
 		formItem.addClassName(GRID_DETAILS_FORM_ITEM.getName());
 		wrapper.addClassName(GRID_DETAILS_FORM.getName());
 		return wrapper;
 	}
 
-	private HorizontalLayout createNameWithDetailsArrow(AttributesClass attributesClass)
+	private RouterLink createNameWithDetailsArrow(AttributesClass attributesClass)
 	{
-		RouterLink label = new RouterLink(attributesClass.getName(), AttributeClassesEditView.class, attributesClass.getName());
-		Icon openIcon = VaadinIcon.ANGLE_RIGHT.create();
-		Icon closeIcon = VaadinIcon.ANGLE_DOWN.create();
-		openIcon.setVisible(!attributesClassGrid.isDetailsVisible(attributesClass));
-		closeIcon.setVisible(attributesClassGrid.isDetailsVisible(attributesClass));
-		openIcon.addClickListener(e -> attributesClassGrid.setDetailsVisible(attributesClass, true));
-		closeIcon.addClickListener(e -> attributesClassGrid.setDetailsVisible(attributesClass, false));
-		return new HorizontalLayout(openIcon, closeIcon, label);
+		return new RouterLink(attributesClass.getName(), AttributeClassesEditView.class, attributesClass.getName());
 	}
 
 }
