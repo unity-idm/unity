b360bb8e73bcce39c6c0e6839a488c34c8b7a22c
Piotr Piernik <piotrpiernik@gmail.com>
2024-04-14 13:55:53 +0200

    Fix verifiable attribute representation

--- vaadin-endpoint-common/src/main/java/io/imunity/vaadin/endpoint/common/plugins/attributes/ext/LayoutWithIcon.java
+++ vaadin-endpoint-common/src/main/java/io/imunity/vaadin/endpoint/common/plugins/attributes/ext/LayoutWithIcon.java
@@ -6,42 +6,15 @@
 package io.imunity.vaadin.endpoint.common.plugins.attributes.ext;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.HasLabel;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import io.imunity.vaadin.elements.InputLabel;
 
-class LayoutWithIcon extends VerticalLayout implements HasLabel
+class LayoutWithIcon extends HorizontalLayout
 {
-	private final Span label;
-
 	LayoutWithIcon(Component representation, Icon icon)
 	{
-		this.label = new InputLabel("");
-		representation.getStyle().set("width", "100%");
-		HorizontalLayout layout = new HorizontalLayout(representation, icon);
-		layout.setPadding(false);
-		layout.setWidthFull();
-		layout.getStyle().set("position", "relative");
-		layout.setAlignItems(Alignment.CENTER);
-
-		icon.getStyle().set("position", "absolute");
-		icon.getStyle().set("left", "102%");
-
-		add(label, layout);
-		setPadding(false);
-		setSpacing(false);
-	}
-
-	@Override
-	public void setLabel(String label) {
-		this.label.setText(label);
-	}
-
-	@Override
-	public String getLabel() {
-		return label.getText();
+		setWidthFull();
+		add(representation, icon);
+		setAlignSelf(Alignment.CENTER, icon);		
 	}
 }
