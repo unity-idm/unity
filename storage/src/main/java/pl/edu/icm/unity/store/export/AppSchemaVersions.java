c693655256cbb361deab5d86bae3c1f86c98249e
Piotr Piernik <piotrpiernik@gmail.com>
2024-04-14 13:07:05 +0200

    UY-1445 add token table indexes

--- storage/src/main/java/pl/edu/icm/unity/store/export/AppSchemaVersions.java
+++ storage/src/main/java/pl/edu/icm/unity/store/export/AppSchemaVersions.java
@@ -29,7 +29,8 @@ public enum AppSchemaVersions
 	V_SINCE_3_8_0(15, "3.8.0"),
 	V_SINCE_3_9_0(16, "3.9.0"),
 	V_SINCE_3_10_0(17, "3.10.0"),
-	V_SINCE_3_12_0(18, "3.12.0");
+	V_SINCE_3_12_0(18, "3.12.0"),
+	V_SINCE_3_16_0(19, "3.16.0");
 	
 	private String name;
 	private int appSchemaVersion;
