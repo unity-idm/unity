c44e9cf97e412d629db99784ad15e1bd88b8ca3b
Piotr Piernik <piotrpiernik@gmail.com>
2024-07-30 13:01:15 +0200

    UY-1467 fix profile view, fix mobile and email attribute representation component

--- vaadin-endpoint-common/src/main/java/io/imunity/vaadin/endpoint/common/plugins/attributes/ext/LayoutWithIcon.java
+++ vaadin-endpoint-common/src/main/java/io/imunity/vaadin/endpoint/common/plugins/attributes/ext/LayoutWithIcon.java
@@ -6,15 +6,40 @@
 package io.imunity.vaadin.endpoint.common.plugins.attributes.ext;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasLabel;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-class LayoutWithIcon extends HorizontalLayout
+import io.imunity.vaadin.elements.InputLabel;
+
+class LayoutWithIcon extends VerticalLayout implements HasLabel
 {
+	private final Span label;
+
 	LayoutWithIcon(Component representation, Icon icon)
 	{
-		setWidthFull();
-		add(representation, icon);
-		setAlignSelf(Alignment.CENTER, icon);		
+		this.label = new InputLabel("");
+		HorizontalLayout layout = new HorizontalLayout(representation, icon);
+		layout.setPadding(false);
+		layout.setWidthFull();
+		layout.setAlignItems(Alignment.CENTER);
+		add(label, layout);
+		setPadding(false);
+		setSpacing(false);
 	}
+
+	@Override
+	public void setLabel(String label)
+	{
+		this.label.setText(label);
+	}
+
+	@Override
+	public String getLabel()
+	{
+		return label.getText();
+	}
+
 }
