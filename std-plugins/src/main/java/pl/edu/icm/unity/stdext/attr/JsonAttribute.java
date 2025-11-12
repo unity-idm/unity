590e5b0f3107375e3344f7904bf658f0b6689f8b
Piotr Piernik <piotrpiernik@gmail.com>
2025-11-12 19:03:12 +0100

    UY-1550 support json attribute

---
+++ std-plugins/src/main/java/pl/edu/icm/unity/stdext/attr/JsonAttribute.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2021 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+
+package pl.edu.icm.unity.stdext.attr;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.Lists;
+
+import pl.edu.icm.unity.base.Constants;
+import pl.edu.icm.unity.base.attribute.Attribute;
+import pl.edu.icm.unity.base.exceptions.InternalException;
+
+public class JsonAttribute
+{
+	public static Attribute of(String name, String groupPath, List<JsonNode> values, String remoteIdp,
+			String translationProfile)
+	{
+		return new Attribute(name, JsonAttributeSyntax.ID, groupPath, toString(values), remoteIdp, translationProfile);
+	}
+
+	public static Attribute of(String name, String groupPath, List<JsonNode> values)
+	{
+		return new Attribute(name, JsonAttributeSyntax.ID, groupPath, toString(values));
+	}
+
+	public static Attribute of(String name, String groupPath, JsonNode... values)
+	{
+		return of(name, groupPath, Lists.newArrayList(values));
+	}
+
+	public static Attribute of(String name, String groupPath, String... values)
+	{
+		return of(name, groupPath, values);
+	}
+
+	private static List<String> toString(List<JsonNode> values)
+	{
+		return values.stream().map(v ->
+				{
+					try
+					{
+						return Constants.MAPPER.writeValueAsString(v);
+					} catch (JsonProcessingException e)
+					{
+						throw new InternalException("Can not convert attribute value to string", e);
+					}
+				})
+				.collect(Collectors.toList());
+	}
+}
---
+++ std-plugins/src/main/java/pl/edu/icm/unity/stdext/attr/JsonAttributeSyntax.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2021 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+
+package pl.edu.icm.unity.stdext.attr;
+
+import java.util.Objects;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.NullNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import pl.edu.icm.unity.base.Constants;
+import pl.edu.icm.unity.base.attribute.IllegalAttributeValueException;
+import pl.edu.icm.unity.base.exceptions.InternalException;
+import pl.edu.icm.unity.engine.api.attributes.AbstractAttributeValueSyntaxFactory;
+import pl.edu.icm.unity.engine.api.attributes.AttributeValueSyntax;
+
+public class JsonAttributeSyntax implements AttributeValueSyntax<JsonNode>
+{
+	public static final String ID = "json";
+
+	public JsonAttributeSyntax()
+	{
+	}
+
+	@Override
+	public String getValueSyntaxId()
+	{
+		return ID;
+	}
+
+	@Override
+	public JsonNode getSerializedConfiguration()
+	{
+		ObjectNode main = Constants.MAPPER.createObjectNode();
+		return main;
+	}
+
+	@Override
+	public void setSerializedConfiguration(JsonNode jsonN)
+	{
+	}
+
+	@Override
+	public void validate(JsonNode value) throws IllegalAttributeValueException
+	{
+	}
+
+	@Component
+	public static class Factory extends AbstractAttributeValueSyntaxFactory<JsonNode>
+	{
+		public Factory()
+		{
+			super(JsonAttributeSyntax.ID, JsonAttributeSyntax::new);
+		}
+	}
+
+	@Override
+	public boolean areEqual(JsonNode value, Object another)
+	{
+		return Objects.equals(value, another);
+	}
+
+	@Override
+	public int hashCode(Object value)
+	{
+		return value.hashCode();
+	}
+
+	@Override
+	public JsonNode convertFromString(String stringRepresentation)
+	{
+		if (stringRepresentation == null)
+			return NullNode.getInstance();
+
+		try
+		{
+			return Constants.MAPPER.readTree(stringRepresentation);
+		} catch (JsonProcessingException e)
+		{
+			throw new InternalException("Invalid Json string", e);
+		}
+	}
+
+	@Override
+	public String convertToString(JsonNode value)
+	{
+		try
+		{
+			return Constants.MAPPER.writeValueAsString(value);
+		} catch (JsonProcessingException e)
+		{
+			throw new InternalException("Can not convert attribute value to string", e);
+		}
+	}
+
+	@Override
+	public boolean isEmailVerifiable()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isUserVerifiable()
+	{
+		return false;
+	}
+}
